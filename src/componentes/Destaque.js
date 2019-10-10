afd7584bf2f3c80a992d1402e9edeeb95edca477
Pedro Gabriel <gaboleal123@gmail.com>
2019-10-10 18:07:44 -0430

    create globalStyle
    creando estilo global

--- src/componentes/Destaque.js
+++ src/componentes/Destaque.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled,{ createGlobalStyle } from 'styled-components'
 
 const Inicio = styled.div`   
 		background: rgba(55,71,79,1);
 		color:white;
+		font-family:'Anton',sans-serif;
 		`
-
+const GlobalStyle = createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css?family=Anton&display=swap');
+	body{
+		margin:0;
+	}
+`;
 
 export default ()=>(
-	<Inicio>Pelicula destacada</Inicio>
+	<div>
+      <GlobalStyle />
+     <Inicio>Pelicula destacada</Inicio>
+    </div>
+	
 	)
